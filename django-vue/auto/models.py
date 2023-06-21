537aec367ff94d0e7e70ecb4adc7aec5ba840a57
NeuralLab <dongogo@gmail.com>
2023-06-21 15:40:05 -0400

    add FedDataRollingPoke table

--- django-vue/auto/models.py
+++ django-vue/auto/models.py
@@ -72,8 +72,16 @@ class FedDataByDay(models.Model):
     mouse = models.ForeignKey(Mouse, on_delete=models.CASCADE)
 
 class FedDataRolling(models.Model):
-    curPoke = models.IntegerField(default=0) # 1 for left, 2 for right
     pokeAcc = models.DecimalField(default=0.0, max_digits=7, decimal_places=6)
+    windowSize = models.IntegerField(default=0)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    fedDate = models.DateField()
+    fedNumDay = models.IntegerField(default=0)
+    mouse = models.ForeignKey(Mouse, on_delete=models.CASCADE)
+
+class FedDataRollingPoke(models.Model):
+    curPoke = models.IntegerField(default=0) # 1 for left, 2 for right
     windowSize = models.IntegerField(default=0)
     startTime = models.DateTimeField()
     endTime = models.DateTimeField()
