22e3ebaa10bb1494f6f67200c3b5a0f25d5c379f
NeuralLab <dongogo@gmail.com>
2025-04-05 00:18:35 -0400

    fix eslint

--- django-vue/frontend/src/components/Edit.vue
+++ django-vue/frontend/src/components/Edit.vue
@@ -68,237 +68,237 @@
 
 <script>
 export default {
-    computed: {
-        editableFields() {
-            return this.fields.filter((field) => field.editable);
-        },
+  computed: {
+    editableFields () {
+      return this.fields.filter((field) => field.editable)
+    }
+  },
+  data () {
+    return {
+      // for cohort dropdown list
+      selectedOption: null,
+      options: [],
+      dropdown_cohort_text: 'Select Cohort',
+      // for mouse list
+      mouse_list: [],
+      fields: [
+        { key: 'mouse_id', label: 'ID' },
+        { key: 'mouse_FED', label: 'FED' },
+        { key: 'mouse_name', label: 'Mouse Name', editable: true },
+        { key: 'mouse_sex', label: 'Sex', editable: true },
+        { key: 'mouse_genotype', label: 'Genotype', editable: true },
+        { key: 'mouse_FED_day', label: 'FED_DAY', tdClass: 'fed_day_sty' }
+      ],
+      // add new cohort
+      new_cohort_name: null,
+      rm_cohort_name: null,
+      // del mouse data
+      del_mouse_id: null,
+      del_start_day: null,
+      del_end_day: null
+    }
+  },
+  methods: {
+    async setCohort (num, name) {
+      this.selectedOption = num
+      this.dropdown_cohort_text = 'Cohort: ' + name
+      await this.updateMouseList(num)
     },
-    data() {
-        return {
-            // for cohort dropdown list
-            selectedOption: null,
-            options: [],
-            dropdown_cohort_text: "Select Cohort",
-            // for mouse list
-            mouse_list: [],
-            fields: [
-                { key: "mouse_id", label: "ID" },
-                { key: "mouse_FED", label: "FED" },
-                { key: "mouse_name", label: "Mouse Name", editable: true },
-                { key: "mouse_sex", label: "Sex", editable: true },
-                { key: "mouse_genotype", label: "Genotype", editable: true },
-                { key: "mouse_FED_day", label: "FED_DAY", tdClass: "fed_day_sty" },
-            ],
-            // add new cohort
-            new_cohort_name: null,
-            rm_cohort_name: null,
-            // del mouse data
-            del_mouse_id: null,
-            del_start_day: null,
-            del_end_day: null,
-        };
+    async refreshMouseList () {
+      await this.updateMouseList(this.selectedOption)
     },
-    methods: {
-        async setCohort(num, name) {
-            this.selectedOption = num;
-            this.dropdown_cohort_text = "Cohort: " + name;
-            await this.updateMouseList(num);
-        },
-        async refreshMouseList() {
-            await this.updateMouseList(this.selectedOption);
-        },
-        async updateMouseList(num) {
-            try {
-                const requestOptions = {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ cohort_id: num }),
-                };
-                const response = await fetch(
-                    "http://128.173.224.170:3000/api/auto/get_mouse_list/",
-                    requestOptions
-                );
-                const data = await response.json();
-                this.mouse_list = data;
-            } catch (error) {
-                console.log(error);
-            }
-        },
-        async delMouseData() {
-            if (
-                confirm(
-                    "Are you sure? Delete Mouse ID=" +
+    async updateMouseList (num) {
+      try {
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ cohort_id: num })
+        }
+        const response = await fetch(
+          'http://128.173.224.170:3000/api/auto/get_mouse_list/',
+          requestOptions
+        )
+        const data = await response.json()
+        this.mouse_list = data
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async delMouseData () {
+      if (
+        confirm(
+          'Are you sure? Delete Mouse ID=' +
                     this.del_mouse_id +
-                    " data from Day " +
+                    ' data from Day ' +
                     this.del_start_day +
-                    " to Day " +
+                    ' to Day ' +
                     this.del_end_day
-                )
-            ) {
-                try {
-                    var delData = {
-                        cohort_id: this.selectedOption,
-                        del_mouse_id: this.del_mouse_id,
-                        del_start_day: this.del_start_day,
-                        del_end_day: this.del_end_day,
-                    };
-                    // console.log(delData)
-                    const requestOptions = {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify(delData),
-                    };
-                    const response = await fetch(
-                        "http://128.173.224.170:3000/api/auto/del_mouse_data/",
-                        requestOptions
-                    );
-                    if (response.status === 201) {
-                        await this.makeToast("Delete: Successful!");
-                        await this.refreshMouseList();
-                    } else {
-                        await this.makeToast("Delete: Failed!");
-                    }
-                } catch (error) {
-                    await this.makeToast("Delete: Failed!");
-                    console.log(error);
-                }
-            }
-        },
-        async saveMouseList() {
-            try {
-                const requestOptions = {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(this.mouse_list),
-                };
-                const response = await fetch(
-                    "http://128.173.224.170:3000/api/auto/put_mouse_list/",
-                    requestOptions
-                );
-                if (response.status === 201) {
-                    await this.makeToast("Save: Successful!");
-                } else {
-                    await this.makeToast("Save: Failed!");
-                }
-            } catch (error) {
-                await this.makeToast("Save: Failed!");
-                console.log(error);
-            }
-        },
-        async addNewCohort() {
-            if (this.new_cohort_name) {
-                try {
-                    const requestOptions = {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify(this.new_cohort_name),
-                    };
-                    const response = await fetch(
-                        "http://128.173.224.170:3000/api/auto/put_new_cohort/",
-                        requestOptions
-                    );
-                    if (response.status === 201) {
-                        await this.makeToast("Add New Cohort: Successful!");
-                        await this.refreshCohortList();
-                    } else {
-                        await this.makeToast("Add New Cohort: Failed!");
-                    }
-                } catch (error) {
-                    await this.makeToast("Add New Cohort: Failed!");
-                    console.log(error);
-                }
-            }
-        },
-        async RmCohort() {
-            if (this.rm_cohort_name) {
-                try {
-                    const requestOptions = {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify(this.rm_cohort_name),
-                    };
-                    const response = await fetch(
-                        "http://128.173.224.170:3000/api/auto/del_cohort/",
-                        requestOptions
-                    );
-                    if (response.status === 201) {
-                        await this.makeToast("Remove Cohort: Successful!");
-                        await this.refreshCohortList();
-                    } else {
-                        await this.makeToast("Remove Cohort: Failed!");
-                    }
-                } catch (error) {
-                    await this.makeToast("Remove Cohort: Failed!");
-                    console.log(error);
-                }
-            }
-        },
-        async refreshCohortList() {
-            fetch("http://128.173.224.170:3000/api/auto/get_cohort_list/")
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error("Network response was not ok");
-                    }
-                    return response.json();
-                })
-                .then((data) => {
-                    this.options = data;
-                })
-                .catch((error) => {
-                    console.error("There was a problem fetching the options:", error);
-                });
-        },
-        async handlePrefillFileUpload(e) {
-            const file = e.target.files[0];
-            const cId = this.selectedOption;
-            const formData = new FormData();
-            formData.append("fileList", file); // Key name should match what your backend expects
-            formData.append("cId", cId);
-
-            const response = await fetch(
-                "http://128.173.224.170:3000/api/auto/upload_prefill_file/",
-                {
-                    method: "POST",
-                    body: formData, // No need to set Content-Type; browser will set it as multipart/form-data
-                }
-            );
-
-            if (response.status === 201) {
-                await this.makeToast("Upload: Successful!");
-                await this.refreshMouseList();
-                //this.upload_error_msgs = 'Upload: Successful!'
-            } else {
-                const edata = await response.json();
-                await this.makeToast("Upload: Failed! Error message: " + edata.message);
-                //this.upload_error_msgs = edata.error + '\n' + edata.message + '\n'
-            }
-        },
-        makeToast(msg, append = true) {
-            this.$bvToast.toast(msg, {
-                title: "Notification",
-                autoHideDelay: 4000,
-                appendToast: append,
-                variant: "primary",
-            });
-        },
+        )
+      ) {
+        try {
+          var delData = {
+            cohort_id: this.selectedOption,
+            del_mouse_id: this.del_mouse_id,
+            del_start_day: this.del_start_day,
+            del_end_day: this.del_end_day
+          }
+          // console.log(delData)
+          const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(delData)
+          }
+          const response = await fetch(
+            'http://128.173.224.170:3000/api/auto/del_mouse_data/',
+            requestOptions
+          )
+          if (response.status === 201) {
+            await this.makeToast('Delete: Successful!')
+            await this.refreshMouseList()
+          } else {
+            await this.makeToast('Delete: Failed!')
+          }
+        } catch (error) {
+          await this.makeToast('Delete: Failed!')
+          console.log(error)
+        }
+      }
     },
-    mounted() {
-        // Make the request to fetch the options
-        fetch("http://128.173.224.170:3000/api/auto/get_cohort_list/")
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                return response.json();
-            })
-            .then((data) => {
-                this.options = data;
-            })
-            .catch((error) => {
-                console.error("There was a problem fetching the options:", error);
-            });
+    async saveMouseList () {
+      try {
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(this.mouse_list)
+        }
+        const response = await fetch(
+          'http://128.173.224.170:3000/api/auto/put_mouse_list/',
+          requestOptions
+        )
+        if (response.status === 201) {
+          await this.makeToast('Save: Successful!')
+        } else {
+          await this.makeToast('Save: Failed!')
+        }
+      } catch (error) {
+        await this.makeToast('Save: Failed!')
+        console.log(error)
+      }
     },
-};
+    async addNewCohort () {
+      if (this.new_cohort_name) {
+        try {
+          const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(this.new_cohort_name)
+          }
+          const response = await fetch(
+            'http://128.173.224.170:3000/api/auto/put_new_cohort/',
+            requestOptions
+          )
+          if (response.status === 201) {
+            await this.makeToast('Add New Cohort: Successful!')
+            await this.refreshCohortList()
+          } else {
+            await this.makeToast('Add New Cohort: Failed!')
+          }
+        } catch (error) {
+          await this.makeToast('Add New Cohort: Failed!')
+          console.log(error)
+        }
+      }
+    },
+    async RmCohort () {
+      if (this.rm_cohort_name) {
+        try {
+          const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(this.rm_cohort_name)
+          }
+          const response = await fetch(
+            'http://128.173.224.170:3000/api/auto/del_cohort/',
+            requestOptions
+          )
+          if (response.status === 201) {
+            await this.makeToast('Remove Cohort: Successful!')
+            await this.refreshCohortList()
+          } else {
+            await this.makeToast('Remove Cohort: Failed!')
+          }
+        } catch (error) {
+          await this.makeToast('Remove Cohort: Failed!')
+          console.log(error)
+        }
+      }
+    },
+    async refreshCohortList () {
+      fetch('http://128.173.224.170:3000/api/auto/get_cohort_list/')
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok')
+          }
+          return response.json()
+        })
+        .then((data) => {
+          this.options = data
+        })
+        .catch((error) => {
+          console.error('There was a problem fetching the options:', error)
+        })
+    },
+    async handlePrefillFileUpload (e) {
+      const file = e.target.files[0]
+      const cId = this.selectedOption
+      const formData = new FormData()
+      formData.append('fileList', file) // Key name should match what your backend expects
+      formData.append('cId', cId)
+
+      const response = await fetch(
+        'http://128.173.224.170:3000/api/auto/upload_prefill_file/',
+        {
+          method: 'POST',
+          body: formData // No need to set Content-Type; browser will set it as multipart/form-data
+        }
+      )
+
+      if (response.status === 201) {
+        await this.makeToast('Upload: Successful!')
+        await this.refreshMouseList()
+        // this.upload_error_msgs = 'Upload: Successful!'
+      } else {
+        const edata = await response.json()
+        await this.makeToast('Upload: Failed! Error message: ' + edata.message)
+        // this.upload_error_msgs = edata.error + '\n' + edata.message + '\n'
+      }
+    },
+    makeToast (msg, append = true) {
+      this.$bvToast.toast(msg, {
+        title: 'Notification',
+        autoHideDelay: 4000,
+        appendToast: append,
+        variant: 'primary'
+      })
+    }
+  },
+  mounted () {
+    // Make the request to fetch the options
+    fetch('http://128.173.224.170:3000/api/auto/get_cohort_list/')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        this.options = data
+      })
+      .catch((error) => {
+        console.error('There was a problem fetching the options:', error)
+      })
+  }
+}
 </script>
 <style>
 .fed_day_sty {
